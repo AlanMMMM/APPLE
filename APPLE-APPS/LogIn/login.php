20a6c6170432a4d2a19b332f8a944cc0fb10e4a3
M Alan <ssingemmmm@gmail.com>
2019-05-06 07:32:22 -0400

    styling

--- APPLE-APPS/LogIn/login.php
+++ APPLE-APPS/LogIn/login.php
@@ -2,30 +2,31 @@
     <title>My Application</title>
     <link rel="stylesheet" href="../../APPLE2/style.css">
 </head>
+<h2>My Application</h2>
 <form action="loginCheck.php" method="post">  
 
     <br />  
     <br />  
     <br />  
-    <br />  
-    <br />  
-    <br />  
-    <br />  
+
     
     
        
-    username:<input type="text" name="username" />  
+    username:<input type="text" name="USERNAME" />
 
     <br />  
 
-    password:<input type="password" name="password" />  
+    password:<input type="password" name="PASSWORD" />
 
     <br />  
 
-    <input type="submit" name="submit" value="log in" />  
+    <input type="submit" name="submit" value="LOG IN" />
 
            
 
-    <a href="register.php">register</a>  
 
+
+</form>
+<form style="text-align: center;"  method="post" action="register.php">
+    <input type="submit" name="go" value="REGISTER">
 </form>
