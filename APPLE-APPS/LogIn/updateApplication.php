52ab7b5c5b509920642636435da8bdefe9c0bc0f
M Alan <ssingemmmm@gmail.com>
2019-05-06 05:22:15 -0400

    update info

--- APPLE-APPS/LogIn/updateApplication.php
+++ APPLE-APPS/LogIn/updateApplication.php
@@ -48,13 +48,159 @@ session_start();
             if($rowTerm=='app_term') {
                 echo "<option value=\"$rowTerm\">" . "Application Semester" . "</option>";
             }
+            if($rowTerm=='app_year') {
+                echo "<option value=\"$rowTerm\">" . "Application Year" . "</option>";
+            }
+            if($rowTerm=='GRE_verbal') {
+                echo "<option value=\"$rowTerm\">" . "GRE Verbal Score" . "</option>";
+            }
+            if($rowTerm=='GRE_quantitative') {
+                echo "<option value=\"$rowTerm\">" . "GRE Quantitative Score" . "</option>";
+            }
+            if($rowTerm=='exam_year') {
+                echo "<option value=\"$rowTerm\">" . "GRE Exam Year" . "</option>";
+            }
+            if($rowTerm=='GRE_score') {
+                echo "<option value=\"$rowTerm\">" . "GRE Subject Test Score" . "</option>";
+            }
+            if($rowTerm=='GRE_subject') {
+                echo "<option value=\"$rowTerm\">" . "GRE Subject" . "</option>";
+            }
+            if($rowTerm=='TOEFL_score') {
+                echo "<option value=\"$rowTerm\">" . "TOEFL Score" . "</option>";
+            }
+            if($rowTerm=='TOEFL_year') {
+                echo "<option value=\"$rowTerm\">" . "TOEFL Exam Year" . "</option>";
+            }
+            if($rowTerm=='bachelor_school') {
+                echo "<option value=\"$rowTerm\">" . "Bachelor Degree School" . "</option>";
+            }
+            if($rowTerm=='bachelor_degree') {
+                echo "<option value=\"$rowTerm\">" . "Bachelor Degree Type" . "</option>";
+            }
+            if($rowTerm=='bachelor_major') {
+                echo "<option value=\"$rowTerm\">" . "Bachelor Degree Major" . "</option>";
+            }
+            if($rowTerm=='bachelor_year') {
+                echo "<option value=\"$rowTerm\">" . "Bachelor Degree Year" . "</option>";
+            }
+            if($rowTerm=='bachelor_gpa') {
+                echo "<option value=\"$rowTerm\">" . "Bachelor Degree GPA" . "</option>";
+            }
+            if($rowTerm=='master_school') {
+                echo "<option value=\"$rowTerm\">" . "Master Degree School" . "</option>";
+            }
+            if($rowTerm=='master_degree') {
+                echo "<option value=\"$rowTerm\">" . "Master Degree Type" . "</option>";
+            }
+            if($rowTerm=='master_major') {
+                echo "<option value=\"$rowTerm\">" . "Master Degree Major" . "</option>";
+            }
+            if($rowTerm=='master_year') {
+                echo "<option value=\"$rowTerm\">" . "Master Degree Year" . "</option>";
+            }
+            if($rowTerm=='master_gpa') {
+                echo "<option value=\"$rowTerm\">" . "Master Degree GPA" . "</option>";
+            }
+            if($rowTerm=='area_of_interest') {
+                echo "<option value=\"$rowTerm\">" . "Area Of Interest" . "</option>";
+            }
+            if($rowTerm=='degree_seeking') {
+                echo "<option value=\"$rowTerm\">" . "Degree Seeking" . "</option>";
+            }
         }
 
         ?>
     </select>
-    <input type="submit" name="semester" value="Select" />
-
+    <input type="submit" name="change" value="Select" />
 </form>
-<br><br><br><br>
+<br><br>
+<form>
+    <?php
+    if(isset($_POST["change"]))
+        $term=$_POST["infoSelection"];
+    if($term=='ssn') {
+        echo "<option value=\"$rowTerm\">" . "Social Security Number" . "</option>";
+    }
+    if($term=='street') {
+        echo "<option value=\"$rowTerm\">" . "Street Address" . "</option>";
+    }
+    if($term=='city') {
+        echo "<option value=\"$rowTerm\">" . "City" . "</option>";
+    }
+    if($term=='state') {
+        echo "<option value=\"$rowTerm\">" . "State" . "</option>";
+    }
+    if($term=='zip') {
+        echo "<option value=\"$rowTerm\">" . "Zip Code" . "</option>";
+    }
+    if($term=='email') {
+        echo "<option value=\"$rowTerm\">" . "Email Address" . "</option>";
+    }
+    if($term=='app_term') {
+        echo "<option value=\"$rowTerm\">" . "Application Semester" . "</option>";
+    }
+    if($term=='app_year') {
+        echo "<option value=\"$rowTerm\">" . "Application Year" . "</option>";
+    }
+    if($term=='GRE_verbal') {
+        echo "<option value=\"$rowTerm\">" . "GRE Verbal Score" . "</option>";
+    }
+    if($term=='GRE_quantitative') {
+        echo "<option value=\"$rowTerm\">" . "GRE Quantitative Score" . "</option>";
+    }
+    if($term=='exam_year') {
+        echo "<option value=\"$rowTerm\">" . "GRE Exam Year" . "</option>";
+    }
+    if($term=='GRE_score') {
+        echo "<option value=\"$rowTerm\">" . "GRE Subject Test Score" . "</option>";
+    }
+    if($term=='GRE_subject') {
+        echo "<option value=\"$rowTerm\">" . "GRE Subject" . "</option>";
+    }
+    if($term=='TOEFL_score') {
+        echo "<option value=\"$rowTerm\">" . "TOEFL Score" . "</option>";
+    }
+    if($term=='TOEFL_year') {
+        echo "<option value=\"$rowTerm\">" . "TOEFL Exam Year" . "</option>";
+    }
+    if($term=='bachelor_school') {
+        echo "<option value=\"$rowTerm\">" . "Bachelor Degree School" . "</option>";
+    }
+    if($term=='bachelor_degree') {
+        echo "<option value=\"$rowTerm\">" . "Bachelor Degree Type" . "</option>";
+    }
+    if($term=='bachelor_major') {
+        echo "<option value=\"$rowTerm\">" . "Bachelor Degree Major" . "</option>";
+    }
+    if($term=='bachelor_year') {
+        echo "<option value=\"$rowTerm\">" . "Bachelor Degree Year" . "</option>";
+    }
+    if($term=='bachelor_gpa') {
+        echo "<option value=\"$rowTerm\">" . "Bachelor Degree GPA" . "</option>";
+    }
+    if($term=='master_school') {
+        echo "<option value=\"$rowTerm\">" . "Master Degree School" . "</option>";
+    }
+    if($term=='master_degree') {
+        echo "<option value=\"$rowTerm\">" . "Master Degree Type" . "</option>";
+    }
+    if($term=='master_major') {
+        echo "<option value=\"$rowTerm\">" . "Master Degree Major" . "</option>";
+    }
+    if($term=='master_year') {
+        echo "<option value=\"$rowTerm\">" . "Master Degree Year" . "</option>";
+    }
+    if($term=='master_gpa') {
+        echo "<option value=\"$rowTerm\">" . "Master Degree GPA" . "</option>";
+    }
+    if($term=='area_of_interest') {
+        echo "<option value=\"$rowTerm\">" . "Area Of Interest" . "</option>";
+    }
+    if($term=='degree_seeking') {
+        echo "<option value=\"$rowTerm\">" . "Degree Seeking" . "</option>";
+    }
+    ?>
+</form>
 </body>
 </html>
