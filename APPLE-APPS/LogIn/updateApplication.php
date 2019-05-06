a52160ff57b654a24c4e94ba07f1407bda6a7278
M Alan <ssingemmmm@gmail.com>
2019-05-06 05:36:53 -0400

    update info

--- APPLE-APPS/LogIn/updateApplication.php
+++ APPLE-APPS/LogIn/updateApplication.php
@@ -9,7 +9,7 @@ session_start();
 </head>
 <body>
 <h2>Update My Application</h2>
-<form style="text-align: center;" action="updateApplicationProcessing" method="post">
+<form style="text-align: center;"  method="post">
     Select A Info Type: <select name="infoSelection" required="required">
         <option disabled selected value> -- select an option -- </option>
         <?php
@@ -120,85 +120,87 @@ session_start();
     if(isset($_POST["change"]))
         $term=$_POST["infoSelection"];
     if($term=='ssn') {
-        echo "<option value=\"$rowTerm\">" . "Social Security Number" . "</option>";
+        echo "<input type=\"number\" required = \"required\" name=\"ssn\" max = \"999999999\" ><br />";
     }
     if($term=='street') {
-        echo "<option value=\"$rowTerm\">" . "Street Address" . "</option>";
+        echo "<input type=\"text\" required = \"required\" name=\"street\" maxlength = \"20\" >";
     }
     if($term=='city') {
-        echo "<option value=\"$rowTerm\">" . "City" . "</option>";
+        echo "<input type=\"text\" required = \"required\" name=\"city\" maxlength = \"15\" >";
     }
     if($term=='state') {
-        echo "<option value=\"$rowTerm\">" . "State" . "</option>";
+        echo "<input type=\"text\" required = \"required\" name=\"state\" maxlength = \"15\" >";
     }
     if($term=='zip') {
-        echo "<option value=\"$rowTerm\">" . "Zip Code" . "</option>";
+        echo "<input type=\"number\" required = \"required\" name=\"zip\" max=\"99999\" min=\"10000\">";
     }
     if($term=='email') {
         echo "<option value=\"$rowTerm\">" . "Email Address" . "</option>";
     }
     if($term=='app_term') {
-        echo "<option value=\"$rowTerm\">" . "Application Semester" . "</option>";
+        echo "<input type=\"radio\" required = \"required\" name=\"app_term\" value=\"Fall\"> Fall<br>
+    <input type=\"radio\" name=\"app_term\" value=\"Spring\"> Spring<br>";
     }
     if($term=='app_year') {
-        echo "<option value=\"$rowTerm\">" . "Application Year" . "</option>";
+        echo "<input type=\"number\" required=required name=\"app_year\" max=\"9999\" min=\"2019\">";
     }
     if($term=='GRE_verbal') {
-        echo "<option value=\"$rowTerm\">" . "GRE Verbal Score" . "</option>";
+        echo "<input type=\"number\"  required = \"required\" name=\"GRE_verbal\" max=\"999\" min=\"0\">";
     }
     if($term=='GRE_quantitative') {
-        echo "<option value=\"$rowTerm\">" . "GRE Quantitative Score" . "</option>";
+        echo "<input type=\"number\" name=\"GRE_quantitative\" max=\"999\" min=\"0\">";
     }
     if($term=='exam_year') {
-        echo "<option value=\"$rowTerm\">" . "GRE Exam Year" . "</option>";
+        echo "<input type=\"number\" name=\"GRE_year\" max=\"2020\" min=\"1000\">";
     }
     if($term=='GRE_score') {
-        echo "<option value=\"$rowTerm\">" . "GRE Subject Test Score" . "</option>";
+        echo "<input type=\"number\" name=\"GRE_score\" max=\"9999\" min=\"0\"/>";
     }
     if($term=='GRE_subject') {
-        echo "<option value=\"$rowTerm\">" . "GRE Subject" . "</option>";
+        echo "<input type=\"text\" name=\"GRE_subject\" maxlength = \"10\" />";
     }
     if($term=='TOEFL_score') {
-        echo "<option value=\"$rowTerm\">" . "TOEFL Score" . "</option>";
+        echo "<input type=\"number\" name=\"TOEFL\" max=\"120\" min=\"0\"/>";
     }
     if($term=='TOEFL_year') {
-        echo "<option value=\"$rowTerm\">" . "TOEFL Exam Year" . "</option>";
+        echo "<input type=\"number\" name=\"TOEFL_year\" max=\"9999\" min=\"1000\"/>";
     }
     if($term=='bachelor_school') {
-        echo "<option value=\"$rowTerm\">" . "Bachelor Degree School" . "</option>";
+        echo "<input type=\"text\" required = \"required\" name=\"bachelor_school\" maxlength = \"25\" />";
     }
     if($term=='bachelor_degree') {
-        echo "<option value=\"$rowTerm\">" . "Bachelor Degree Type" . "</option>";
+        echo "<input type=\"text\" required = \"required\" name=\"bachelor_degree\" maxlength = \"25\" />";
     }
     if($term=='bachelor_major') {
-        echo "<option value=\"$rowTerm\">" . "Bachelor Degree Major" . "</option>";
+        echo "<input type=\"text\" required = \"required\" name=\"bachelor_major\" maxlength = \"25\" />";
     }
     if($term=='bachelor_year') {
-        echo "<option value=\"$rowTerm\">" . "Bachelor Degree Year" . "</option>";
+        echo "<input type=\"number\" required = \"required\" name=\"bachelor_year\" max=\"2020\" min=\"1000\" >";
     }
     if($term=='bachelor_gpa') {
-        echo "<option value=\"$rowTerm\">" . "Bachelor Degree GPA" . "</option>";
+        echo "<input type=\"number\" step=\"0.01\" required = \"required\" name=\"bachelor_gpa\" />";
     }
     if($term=='master_school') {
-        echo "<option value=\"$rowTerm\">" . "Master Degree School" . "</option>";
+        echo "<input type=\"text\" name=\"masters_school\" maxlength = \"25\" />";
     }
     if($term=='master_degree') {
-        echo "<option value=\"$rowTerm\">" . "Master Degree Type" . "</option>";
+        echo "<input type=\"text\" name=\"masters_degree\" maxlength = \"25\" />";
     }
     if($term=='master_major') {
-        echo "<option value=\"$rowTerm\">" . "Master Degree Major" . "</option>";
+        echo "<input type=\"text\" name=\"masters_major\" maxlength = \"25\" />";
     }
     if($term=='master_year') {
-        echo "<option value=\"$rowTerm\">" . "Master Degree Year" . "</option>";
+        echo "<input type=\"number\" name=\"masters_year\" max=\"2020\" min=\"1000\">";
     }
     if($term=='master_gpa') {
-        echo "<option value=\"$rowTerm\">" . "Master Degree GPA" . "</option>";
+        echo "<input type=\"number\" step=\"0.01\" name=\"masters_gpa\" />";
     }
     if($term=='area_of_interest') {
-        echo "<option value=\"$rowTerm\">" . "Area Of Interest" . "</option>";
+        echo "<input type=\"text\" required = \"required\" name=\"area_of_interest\"  maxlength = \"25\">";
     }
     if($term=='degree_seeking') {
-        echo "<option value=\"$rowTerm\">" . "Degree Seeking" . "</option>";
+        echo "<input type=\"radio\" required = \"required\" name=\"degree\" value=\"MS\"> MS<br>
+    <input type=\"radio\" name=\"degree\" value=\"PHD\"> PHD<br>";
     }
     ?>
 </form>
